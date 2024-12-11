bf83b08b87878ebe017eadb07b5e25aeab0f6598
Daniel Urbina <edu211004@gmail.com>
2024-12-11 01:04:53 -0600

    broken

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { useReducer } from 'react'
+import { useContext, useReducer } from 'react'
+import CounterContext from './context/CounterContext'
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
+
 function App() {
   const [state, dispatch] = useReducer(counterReducer, 5)
-  
+  const context = useContext(CounterContext)
+  console.log(context.name)
+
+
   return (
     <div>
       <h2>useReducer usage</h2>
       <p>{state}</p>
-      <button onClick={()=> dispatch({type:'INCREMENT'})}>+</button>
-      <button onClick={()=> dispatch({type:'DECREMENT'})}>-</button>
-      <button onClick={()=> dispatch({type:'ZERO'})}>0</button>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
+      <button onClick={() => dispatch({ type: 'ZERO' })}>0</button>
     </div>
   )
 }
---
+++ src/context/CounterContext.jsx
@@ -0,0 +1,18 @@
+import { Children, createContext } from "react";
+
+const counterReducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return state + 1
+      case 'DECREMENT':
+        return state - 1
+      case 'ZERO':
+        return 0
+      default:
+        return state
+    }
+  }
+
+const CounterContext = createContext({children})
+
+export default CounterContext
